8eef0b7bbbd79739e117cd9621375d5c02c7cc57
mhmyzah <xjmxjm00@gmail.com>
2020-10-02 17:30:35 +0300

    g
    g

---
+++ 3d adevnture/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = new Vector3(0, 0,0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) { rot.z = rot.z + .1f;
+            Debug.Log(rot.z);
+        }
+        // gameObject.transform.rotation.Set(Input.mousePosition.x, Input.mousePosition.y,0,0);
+        gameObject.transform.position = rot;
+
+      
+
+    }
+}
