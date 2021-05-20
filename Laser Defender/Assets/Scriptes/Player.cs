d7efe3c8e24023c9c3d7913f77e0e9d69144f3d9
mhmyzah <xjmxjm00@gmail.com>
2021-05-21 02:30:41 +0300

    t

--- Laser Defender/Assets/Scriptes/Player.cs
+++ Laser Defender/Assets/Scriptes/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float min = -5;
+    [SerializeField] float max = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +24,16 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-          var deltaX = Input.GetAxis("Horizontal");
+        /*  var deltaX = Input.GetAxis("Horizontal");
            var newXPos = transform.position.x + deltaX;
            transform.position = new Vector2(newXPos, transform.position.y);
-       /* var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, newYPos);*/
+     */
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+         var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, min, max);
+
+         transform.position = new Vector2(newXPos, newYPos);
     }
 }
 
